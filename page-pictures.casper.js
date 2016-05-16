eee2260aa9b4571ed96e115189973281429508e3
Sang-Hoon RHEE <shr386@hotmail.com>
2016-05-17 01:27:48 +0900

    .

--- page-pictures.casper.js
+++ page-pictures.casper.js
@@ -1,172 +1,147 @@
-#!/usr/bin/env casperjs
+#!/bin/sh
+//bin/true; exec casperjs --web-security=false "$0" "$@"
+//bin/true; exec casperjs --web-security=false --verbose --log-level=info "$0" "$@"
+//bin/true; exec casperjs --web-security=false --verbose --log-level=info --proxy=127.0.0.1:9050 --proxy-type=socks5 "$0" "$@"
+(function () {
 
-var casper = require('casper').create({
-    //verbose: true,
-    logLevel: "debug",
-    pageSettings: { webSecurityEnabled: false }
-});
+    var casper = require('casper').create({
+        //verbose: true,
+        //logLevel: "info",
+        pageSettings: { webSecurityEnabled: false },
+        clientScripts: ["spark-md5.js"]
+    });
 
-var casper_utils_dump = require('utils').dump;
+    var urls = casper.cli.args.slice(0);
 
-var config = {
-    agent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36',
-    timeout: 15000, /* 5s ? */
-    minSize: 170000, /* 170k ? */
-    minWidth: 1080,
-    minHeight: 1080,
-    minPixels: 1920 * 1080,
-}
+    var utils = require('utils'),
+        SparkMD5 = require('spark-md5');
 
-function casper_dump_indices(obj, tag) {
-    if (obj) {
-	var indices = [];
-	for (var i in obj) {
-	    indices.push(i);
-	}
-	casper.echo('=== dump_indices ===: ' + tag);
-	casper_utils_dump(indices);
-    }
-}
-
-function casper_hash_string(str) {
-    var hashCode = function (str) {
-	var hash = 0;
-	if (str.length == 0) return hash;
-	for (i = 0; i < str.length; i++) {
-	    char = str.charCodeAt(i);
-	    hash = ((hash << 5) - hash) + char;
-	    hash = hash & hash; // Convert to 32bit integer
-	}
-	return hash;
-    }, num = hashCode(str);
-    return (num < 0 ? (0xFFFFFFFF + num + 1) : num).toString(16);
-}
-
-function casper_download_image(src, mimeType) {
-
-    var basename = src.split(/[?#]/).shift().split(/[\/]/).pop(), /* basename() */
-    filename = '' + basename + '_' + casper_hash_string(src) + '.png';
-    casper.echo('casper_download_image: ' + src + ' ' + filename);
-    try {
-	casper.download(src, filename);
-    } catch (e) {
-	/* */
+    var config = {
+        agent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.94 Safari/537.36',
+        timeout: 15000, /* 5s ? */
+        minSize: 170000, /* 170k ? */
+        minWidth: 1080,
+        minHeight: 1080,
+        minPixels: 1920 * 1080,
     }
 
-}
-
-function casper_short_url(url) {
-    if ( url.length > 200 ) {
-	return url.substr(0,200) + ' ...';
-    } else {
-	return url;
+    function casper_basename(path) {
+        var basename = path.split(/[?#]/).shift().split(/[\/]/).pop(); /* basename() */
+        return basename;
     }
-}
 
-function casper_dump_backtrace(msg, backtrace) {
-    casper.echo('backtrace: ' + msg);
-    for (var i = 0; i < backtrace.length; i++) {
-	var f = backtrace[i];
-	casper.echo('    ' + f.file + ':' + f.line + ': ' + f.function);
+    function casper_abbrev_url(url, limit) {
+        limit = limit || 100;
+        return (url.length > limit) ? '... ' + casper_basename(url) : url;
     }
-}
 
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////
+    function casper_dump_shallow(object, title) {
+        casper.echo(title + ':');
+        for (var k in object) {
+            casper.echo('    ' + k + ': ' + JSON.stringify(object[k]));
+        }
+    }
 
-var urls = casper.cli.args.slice(0);
-var casper_image_resources = {};
-var images = {};
+    function casper_dump_backtrace(msg, backtrace) {
+        casper_dump_shallow(backtrace, msg);
+    }
 
-casper.options.waitTimeout = config.timeout;
-casper.userAgent(config.agent);
+    function casper_download_image(src, mimeType, hash, outdir) {
 
-casper.on('remote.message', function (message) { this.echo('message: ' + message) });
-
-casper.on('error', function (msg, backtrace) { casper_dump_backtrace('error: ' + msg, backtrace) });//this.echo('error: '+msg); dump(backtrace[0]) });
-casper.on('page.error', function (msg, backtrace) { casper_dump_backtrace('page.error: ' + msg, backtrace) });//this.echo('page.error:'+msg); dump(backtrace[0]) });
-casper.on('complete.error', function (err) { this.echo('complete.error: ' + JSON.stringify(err)) });
-
-casper.on('resource.error', function (resourceError) {
-    //var errorCode = resourceError.errorCode,
-    //	errorString = resourceError.errorString,
-    //	url = resourceError.url,
-    //	id = resourceError.id;
-    this.echo('resource.error: ' + JSON.stringify(resourceError));
-})
-
-casper.on('step.error', function (err) {
-    this.echo('step.error: ' + JSON.stringify(err));
-})
-
-casper.on('resource.received', function (resource) {
-
-    /// collect url into casper_image_resources, if contentType bodySize matches
-
-    //this.echo('resource.recevied: ' + resource.contentType + ' ' + resource.bodySize + ' ' + casper_short_url(resource.url));
-
-    if (/^image\//.test(resource.contentType)) {
-
-	if (typeof resource.bodySize == 'undefined' || resource.bodySize > config.minSize) {
-
-	    //this.echo('resource.recevied: ' + resource.contentType + ' ' + resource.bodySize + ' ' + casper_short_url(resource.url));
-	    casper_image_resources[resource.url] = resource.contentType;
-
-	}
+        var basename = casper_basename(src),
+            ext = mimeType.split(/\//).pop().toLowerCase(),
+            filename = (outdir ? outdir : '') + (hash ? hash : basename + '_' + SparkMD5.hash(src, false)) + '.' + ext;
+        casper.echo('casper_download_image: ' + casper_abbrev_url(src) + ' ' + filename);
+        try {
+            casper.download(src, filename);
+        } catch (e) {
+            /* */
+        }
 
     }
 
-});
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////
 
-casper.start().eachThen(urls, function (response) {
-    this.thenOpen(response.data);
+    var casper_image_resources = {};
 
-    this.then(function () {
+    casper.options.waitTimeout = config.timeout;
+    casper.userAgent(config.agent);
 
-	this.evaluate(function (resources, config) {
+    casper.on('remote.message', function (message) { this.echo('message: ' + message) });
 
-	    window.collected_images = {};
+    casper.on('error', function (msg, backtrace) { casper_dump_backtrace(msg, backtrace) });
+    casper.on('page.error', function (msg, backtrace) { casper_dump_backtrace(msg, backtrace) });
+    casper.on('complete.error', function (err) { this.echo('complete.error: ' + JSON.stringify(err)) });
+    casper.on('resource.error', function (resourceError) { this.echo('resource.error: ' + resourceError.errorString + ' ' + casper_abbrev_url(resourceError.url, 50)) }); //{ casper_dump_shallow(resourceError,'resource.error'); });
+    casper.on('step.error', function (err) { this.echo('step.error: ' + JSON.stringify(err)); });
 
-	    for (url in resources) {
-		!function (url, mimeType) {
-		    var img = new Image();
-		    img.onLoad = function () {
-			if (img.width >= config.minWidth &&
-			    img.height >= config.minHeight &&
-			    img.width * img.height >= config.minPixels) {
-			    window.collected_images[url] = mimeType;
-			}
-		    }
-		    img.src = url;
-		    if (img.complete || img.readyState === 4) {
-			img.onLoad();
-		    }
-		} (url, resources[url]);
-	    }
+    casper.on('resource.received', function (resource) {
+        /// collect url into casper_image_resources, if contentType bodySize matches
+        if (/^image\//.test(resource.contentType)) {
+            if (typeof resource.bodySize == 'undefined' || resource.bodySize > config.minSize) {
+                casper_image_resources[resource.url] = resource.contentType;
+            }
+        }
+    });
 
-	}, casper_image_resources, config);
+    casper.start().eachThen(urls, function (response) {
+
+        this.thenOpen(response.data);
+        this.then(function () {
+            this.evaluate(function (resources, config) {
+                window.collected_images = {};
+                for (url in resources) {
+                    !function (url, mimeType) {
+                        var img = new Image();
+                        img.onLoad = function () {
+                            var hash;
+                            if (img.width >= config.minWidth &&
+                                img.height >= config.minHeight &&
+                                img.width * img.height >= config.minPixels) {
+                                // XXX check, takes too long ???
+                                hash = (function (src) {
+                                    var tstart = Date.now(),
+                                    hash = SparkMD5.hashBinary(__utils__.getBinary(src), false),
+                                    telapsed = Date.now() - tstart;
+                                    console.log('hashBinary elapsed: ' + telapsed + ' src=' + src);
+				    return hash;
+                                })(img.src);
+                                window.collected_images[url] = {
+                                    mimeType: mimeType,
+                                    hash: hash
+                                };
+                            }
+                        }
+                        img.src = url;
+                        if (img.complete || img.readyState === 4) {
+                            img.onLoad();
+                        }
+                    } (url, resources[url]);
+                }
+            }, casper_image_resources, config);
+        });
+        this.then(function () {
+            var images = this.evaluate(function () { return window.collected_images });
+            //this.echo('=== end of scan ===: images=' + JSON.stringify(images));
+            casper_dump_shallow(images, '=== end of scan ===');
+            for (url in images) {
+                var info = images[url];
+                casper_download_image(url, info.mimeType, info.hash, '1/');
+            }
+        });
 
     });
 
-    this.then(function () {
+    casper.run()
 
-	var images = this.evaluate(function () { return window.collected_images });
+})();
 
-	this.echo('end of scan: images=' + JSON.stringify(images));
-
-	for (url in images) {
-	    var mimeType = images[url];
-	    casper_download_image(url, mimeType);
-	}
-
-    });
-
-});
-
-casper.run()
 // Emacs:
 // mode: javascript
 // c-basic-offset: 4
+// tab-width: 4
+// indent-tabs-mode: nil
 // End:
 // vim: se ft=javascript st=4 ts=8 sts=4
---
+++ test-2.sh
@@ -0,0 +1,10 @@
+:
+./page-pictures.casper.js \
+"http://blog.daum.net/_blog/BlogTypeView.do?blogid=0q54d&articleno=122&categoryId=6&regdt=20151115081548&totalcnt=25" \
+"http://seokblog.tistory.com/117" \
+"http://seokblog.tistory.com/118" \
+"http://quasarzone.co.kr/bbs/board.php?bo_table=qb_image&wr_id=91" \
+"http://aion.plaync.com/board/server/view?articleID=3103823&page=&rootCategory=28&category=1411" \
+"http://tcafeuu.com/bbs/board.php?bo_table=c_jjalbang&wr_id=97970" \
+"http://www.onlifezone.com/soul/textyle/14914311" \
+"https://tailstar.net/index.php?mid=gallery_enter&document_srl=1746645&cpage=1"
